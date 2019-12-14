79f57c7520949209aff941e31c0a1549851d1665
Spencer Dent <mr.spencerdent@gmail.com>
2019-12-14 10:58:47 -0500

    cleans up day 1

--- src/day1/fuel.js
+++ src/day1/fuel.js
@@ -1,7 +1,15 @@
 import {
-  compose
+  compose,
+  map,
+  split,
+  trim,
+  sum
 } from 'ramda'
 
 export const moduleFuel = compose(n => n - 2, Math.floor, n => n / 3)
 
 export const fuelFuel = amount => moduleFuel(amount) <= 0 ? 0 : moduleFuel(amount) + fuelFuel(moduleFuel(amount))
+
+export const parseModuleArray = str => map(Number.parseInt)(compose(split('\n'), trim)(str))
+
+export const calc = fn => arr => sum(map(fn)(arr))
--- src/day1/fuelCalculator.js
+++ src/day1/fuelCalculator.js
@@ -1,32 +1,25 @@
 import {
-  readFile,
+  readFile
 } from 'fs'
 
 import {
-  compose,
-  map,
-  sum
-} from 'ramda'
-
-import {
-  fuelFuel, moduleFuel
+  moduleFuel,
+  fuelFuel,
+  parseModuleArray,
+  calc
 } from './fuel'
 
 import {
-  toNum,
-  split,
-  trim
+  fileOpts
 } from '../utils/utils'
 
-const splitLines = split('\n')
-
-readFile('./res/day1/fuel_input.txt', { encoding: 'ascii', flag: 'r' }, (err, data) => {
+readFile('./res/day1/fuel_input.txt', fileOpts, (err, data) => {
   if (err) throw err
 
-  const array = map(toNum)(compose(splitLines, trim)(data))
+  const array = parseModuleArray(data)
 
-  const baseFuel = sum(map(moduleFuel)(array))
-  const recurFuel = sum(map(fuelFuel)(array))
+  const baseFuel = calc(moduleFuel)(array)
+  const recurFuel = calc(fuelFuel)(array)
 
   console.log(`Fuel for modules: ${baseFuel}`)
   console.log(`Fuel for fuel and modules: ${recurFuel}`)
